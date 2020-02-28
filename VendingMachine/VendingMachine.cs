4e5fd0e198c394bd78a275e413e63fb55d8a74e9
mark vanzuela <mark.vanzuela@devpartners.co>
2020-02-28 14:35:49 +0800

    Add project files.

---
+++ VendingMachine/VendingMachine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine
+{
+    public class VendingMachine
+    {
+        private decimal Balance => 0.0M;
+        public  decimal GetBalance()
+        {
+            return Balance;
+        }
+    }
+}
---
+++ VendingMachine/VendingMachineTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace VendingMachine
+{
+    [TestFixture]
+    public class VendingMachineTest
+    {
+        [Test]
+        public void WhenVendingMachineStarts_ThenBalanceShouldBeZero()
+        {
+            var machine = new VendingMachine();
+
+            Assert.AreEqual(0, machine.GetBalance());
+        }
+    }
+}
