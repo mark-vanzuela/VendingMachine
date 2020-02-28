c33e0cb34318eae0ce3c6238a6aa852f10fb9d10
mark vanzuela <mark.vanzuela@devpartners.co>
2020-02-28 15:16:20 +0800

    add valid cash amounts

--- VendingMachine/VendingMachine.cs
+++ VendingMachine/VendingMachine.cs
@@ -6,10 +6,33 @@ namespace VendingMachine
 {
     public class VendingMachine
     {
-        private decimal Balance => 0.0M;
+        private decimal Balance { get; set;  }
+
         public  decimal GetBalance()
         {
             return Balance;
+        }
+
+        public  void AddCash(decimal value)
+        {
+            switch (value)
+            {
+                case 100.0M:
+                case 50.0M:
+                case 20.0M:
+                case 10.0M:
+                case 5.0M:
+                case 1.0M:
+                case 0.50M:
+                case 0.25M:
+                    Balance += value;
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid value");
+
+            }
+
+            
         }
     }
 }
--- VendingMachine/VendingMachineTest.cs
+++ VendingMachine/VendingMachineTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
 using NUnit.Framework;
 
 namespace VendingMachine
@@ -12,6 +13,37 @@ namespace VendingMachine
             var machine = new VendingMachine();
 
             Assert.AreEqual(0, machine.GetBalance());
+        }
+
+        [TestCase(100.0)]
+        [TestCase(50.0)]
+        [TestCase(20.0)]
+        [TestCase(10.0)]
+        [TestCase(5.0)]
+        [TestCase(1.0)]
+        [TestCase(0.5)]
+        [TestCase(0.25)]
+        public void WhenAddingCash_ThenItOnlyAccepts_100_50_20_10_5_1_050_025(decimal expected)
+        {
+            var machine = new VendingMachine();
+
+            machine.AddCash(expected);
+
+            Assert.AreEqual(expected, machine.GetBalance());
+
+        }
+
+        [TestCase(500.0)]
+        [TestCase(25.0)]
+        [TestCase(8.0)]
+        [TestCase(0.75)]
+        public void WhenAddingInvalidCashAmount_ThenItShouldThrowException(decimal expected)
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var machine = new VendingMachine();
+                machine.AddCash(expected);
+            });
         }
     }
 }
